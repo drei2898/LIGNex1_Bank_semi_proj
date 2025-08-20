7a779411dffa458665a519b859255db289bf1dfe
DESKTOP-B61NMIF\DSO9 <kim.seokho1012@gmail.com>
2025-08-20 11:10:56 +0900

    Docs: revise gui_ex.txt file

--- GUI/gui.cpp
+++ GUI/gui.cpp
@@ -1,2 +1,26 @@
-#include <iostream>
+#include "gui.h"
 using namespace std;
+
+void displayTitle(void)
+{
+}
+
+void displayLogin(void)
+{
+}
+
+void displayMenu(void)
+{
+}
+
+void displayCreate(void)
+{
+}
+
+void displayDeposit(void)
+{
+}
+
+void displayAttend(void)
+{
+}
--- GUI/gui.h
+++ GUI/gui.h
@@ -1,2 +1,8 @@
 #include <iostream>
 #include <vector>
+
+void displayLogin(void);
+void displayMenu(void);
+void displayCreate(void);
+void displayDeposit(void);
+void displayAttend(void);
